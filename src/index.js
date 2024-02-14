b3fd1dfe65982602603f7d21d77ec27d9874a554
pedroara <araujo.peo@gmail.com>
2024-02-14 17:32:05 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      console.log("event", event, "\n\n", "event?.data", event?.data);
+    });
+    return () => {
+      window.removeEventListener("message", () => {});
+    };
+  }, []);
+
+  return (
+    <iframe
+      src="https://player.scaleup.com.br/embed/48c6ba5ea33f2f854ffda92294c5cfd18539f451"
+      title="Teste"
+      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen=""
+      style={{
+        width: "100%",
+        aspectRatio: "16 / 9",
+        border: "0px",
+        margin: "0px auto",
+      }}
+    ></iframe>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

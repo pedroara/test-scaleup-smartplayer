6a54c152a3776fb5ba38d93a9bc31cf748b05a94
pedroara <araujo.peo@gmail.com>
2024-03-04 13:48:24 -0300

    update: change debug parameters

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <iframe
-      src="https://player.scaleup.com.br/embed/48c6ba5ea33f2f854ffda92294c5cfd18539f451?events=controlsdisabled,controlsenabled,dispose,durationchange,ended,enterFullWindow,enterpictureinpicture,error,exitFullWindow,firstplay,fullscreenchange,leavepictureinpicture,loadedmetadata,loadstart,pause,play,playerreset,playerresize,playing,posterchange,progress,ratechange,ready,resize,seeked,seeking,textdata,timeupdate,useractive,userinactive,volumechange,waiting"
+      src="https://player.scaleup.com.br/embed/c39a428ec72dbec705f7140da444c2faf7357ffe?events=ended,progress,firstplay&seekTo=0&fullscreen=1&controls=1&watermarkText=Pedro Araújo Pinto - 07560191509&watermarkPosition=top-left&watermarkOpacity=0.5&watermarkTime=20&watermarkType=text&"
       title="Teste"
       allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen=""
